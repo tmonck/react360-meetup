3cf76303bb9a9ff02e2bb330854b4290e6b30a02
Tom Monck <junja430@gmail.com>
2019-02-07 19:30:37 -0500

    Initial commit.

---
+++ client.js
@@ -0,0 +1,28 @@
+// This file contains the boilerplate to execute your React app.
+// If you want to modify your application's content, start in "index.js"
+
+import {ReactInstance, Location} from 'react-360-web';
+
+function init(bundle, parent, options = {}) {
+  const r360 = new ReactInstance(bundle, parent, {
+    // Add custom options here
+    fullScreen: true,
+    ...options,
+  });
+
+  // Render your app content to the default cylinder surface
+  r360.renderToSurface(
+    r360.createRoot('Hello360', { /* initial props */ }),
+    r360.getDefaultSurface()
+  );
+  
+  r360.renderToLocation(
+    r360.createRoot('Chewy'),
+    new Location([0, -2, -10]),
+  );
+
+  // Load the initial environment
+  r360.compositor.setBackground(r360.getAssetURL('falcon_cockpit_v005_1500_.jpg'));
+}
+
+window.React360 = {init};
---
+++ index.js
@@ -0,0 +1,8 @@
+import {
+  AppRegistry
+} from 'react-360';
+import Hello360 from './Hello360';
+import Chewy from './chewy';
+
+AppRegistry.registerComponent('Hello360', () => Hello360);
+AppRegistry.registerComponent('Chewy', () => Chewy);
